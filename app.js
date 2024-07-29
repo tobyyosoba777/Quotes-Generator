c53b0687a61d6dc7e3c4cb1aa6eb85df6b9cb609
tobiloba <tobiosoba777@gmail.com>
2024-07-28 18:07:16 -0700

    Added a new quote

--- app.js
+++ app.js
@@ -442,6 +442,10 @@ const quotes = [
         quote: `“ The measure of intelligence is the ability to change ”`,
         author: "Albert Einstein"
     },
+    {
+        quote: `“ Don’t be afraid to give up the good to go for the great ”`,
+        author: "John D. Rockefeller"
+    },
     
 ]
 
