ea4f1f4a2ebb7a814fb71e8c80056f0f4f908bfd
tobiloba <tobiosoba777@gmail.com>
2024-09-04 17:05:18 -0700

    Added a new quote

--- app.js
+++ app.js
@@ -584,6 +584,10 @@ const quotes = [
         unstoppable determination. ”`,
         author: " Ralph Marston "
     },
+    {
+        quote: `“ There is only one way to happiness and that is to cease worrying about things which are beyond the power of our will. ”`,
+        author: " ~Unknown "
+    },
     
 ]
 
