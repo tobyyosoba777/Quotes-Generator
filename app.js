015b08ad5f8f4dc1753955feb16cb2565cead718
tobiloba <tobiosoba777@gmail.com>
2024-10-10 16:47:56 -0700

    Added a new quote

--- app.js
+++ app.js
@@ -701,6 +701,10 @@ const quotes = [
         quote: `“ You have power over your mind - not outside events. Realize this, and you will find strength. ”`,
         author: "  Marcus Aurelius, Meditations  "
     },
+    {
+        quote: `“ It's not the size of the dog in the fight, it's the size of the fight in the dog. ”`,
+        author: " Mark Twain  "
+    },
     
 ]
 
