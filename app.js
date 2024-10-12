2631f9f8a94f9ab68c2a29b5b9db3e6cae239f2d
tobiloba <tobiosoba777@gmail.com>
2024-10-11 17:53:14 -0700

    Added a new quote

--- app.js
+++ app.js
@@ -702,8 +702,8 @@ const quotes = [
         author: "  Marcus Aurelius, Meditations  "
     },
     {
-        quote: `“ It's not the size of the dog in the fight, it's the size of the fight in the dog. ”`,
-        author: " Mark Twain  "
+        quote: `“ The only impossible journey is the one you never begin. ”`,
+        author: " Tony Robbins "
     },
     
 ]
