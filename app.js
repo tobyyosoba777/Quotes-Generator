e738ca882ebc60df5349aedadecb4d1718274390
tobiloba <tobiosoba777@gmail.com>
2024-10-18 19:29:57 -0700

    Added a new quote

--- app.js
+++ app.js
@@ -740,7 +740,7 @@ const quotes = [
     },
     {
         quote: `“ To live is the rarest thing in the world. Most people exist, that is all. ”`,
-        author: " Oscar Wilde. "
+        author: " Oscar Wilde.. "
     },
     
 ]
